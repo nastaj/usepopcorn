1f0de61bee822da6ae07cb8e2d4f950d5590caa5
nastaj <G00424689@atu.ie>
2023-08-24 21:42:30 +0200

    selecting movie & movie details

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+// import StarRating from "./StarRating";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+
+//   return (
+//     <div>
+//       <StarRating onSetRating={setMovieRating} />
+//       <p>This movie has been rated {movieRating}</p>
+//     </div>
+//   );
+// }
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test /> */}
+  </React.StrictMode>
+);
